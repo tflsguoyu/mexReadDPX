68d90b41bf20b0b6012455526c67783b988133d5
GuoYu <tflsguoyu@gmail.com>
2015-02-03 14:25:32 +0800

    read dpx file

---
+++ README.txt
@@ -0,0 +1,65 @@
+mexReadDPX
+
+=======================
+
+Read DPX file in MATLAB using MATLAB MEX functions
+
+=======================
+
+Here I provide two MEX functions to read DPX file in MATLAB,
+
+1. image = mexReadDPX8(filename) 
+"mexReadDPX8" reads a 8-bits/10-bits/12-bits/16-bits DPX image from a file specified by the string "filename".
+The return value "image" is an H-by-W-by-3 array in format UINT8 containing the image data.
+ 
+2. image = mexReadDPX16(filename) 
+"mexReadDPX8" reads a 8-bits/10-bits/12-bits/16-bits DPX image from a file specified by the string "filename".
+The return value "image" is an H-by-W-by-3 array in format UINT16 containing the image data.
+
+Function "mexReadDPX8" will compress the input DPX file to 8 bits, that means for each color channel, the color value will range from 0 to 255. But function "mexReadDPX16" will re-range the input DPX file to 16 bits, that means for each color channel, the color value will range from 0 to 65535.
+
+If you don't want to have quality loss, please use "mexReadDPX16".
+
+========================
+
+MEX file generation platform:
+1. Window8 x64; 
+2. Matlab 2014b x64;
+3. Visual Studio 2012
+
+If you are working on other platform, you may need to re-compile 
+"mexReadDPX8.mexw64" 
+		or 
+"mexReadDPX16.mexw64"
+
+Run this in MATLAB command window:
+mex mexReadDPX8.cpp DPX.cpp DPXHeader.cpp InStream.cpp Reader.cpp Codec.cpp ElementReadStream.cpp
+		or
+mex mexReadDPX16.cpp DPX.cpp DPXHeader.cpp InStream.cpp Reader.cpp Codec.cpp ElementReadStream.cpp
+
+=========================
+
+Here I provide two DPX file: 
+"imageFromBlackmagic.dpx"
+"imageFromWebsite.dpx"
+
+Example of how to use it:
+1. add "mexReadDPX8.mexw64" or "mexReadDPX8.mexw64" to your MATLAB searching path.
+2. read DPX file in 8 bits:
+		imageA = mexReadDPX8('imageFromBlackmagic.dpx');
+   Now you get an array in format "1080x1920x3 uint8".
+3. display this image: 
+		figure;imshow(imageA);
+4. read DPX file in 16 bits:
+		imageB = mexReadDPX16('imageFromWebsite.dpx');
+   Now you get an array in format "1556x2048x3 uint16".
+5. display this image: 
+		figure;imshow(imageB);
+		
+		
+===========================
+
+This code is based on SMPTE DPX v2 Image Format reader/writer
+https://code.google.com/p/dpx/
+
+If you have any question and feedback, please send email to tflsguoyu@gmail.com
---
+++ mexReadDPX16.cpp
@@ -0,0 +1,124 @@
+// run this:
+// mex mexReadDPX16.cpp DPX.cpp DPXHeader.cpp InStream.cpp Reader.cpp Codec.cpp ElementReadStream.cpp
+
+#include "mex.h"
+#include "matrix.h"
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "DPX.h"
+
+using namespace std;
+
+void readDPXresolution(char* filename, int& height, int& width)
+{
+    // open the image
+	InStream img;
+	if (!img.Open(filename))
+	{
+		cout << "Unable to open file " << filename << endl;
+	}
+    
+    dpx::Reader dpx;
+	dpx.SetInStream(&img);
+	if (!dpx.ReadHeader())
+	{
+		cout << "Unable to read header" << endl;
+	}
+    
+    height = dpx.header.Height();
+	width = dpx.header.Width();
+	
+	img.Close();
+    
+}
+
+void readDPX(char* filename, char* arr)
+{
+	int offset = 1;
+	bool write8bit = false;
+// 	bool write8bit = true;
+
+	// open the image
+	InStream img;
+	if (!img.Open(filename))
+	{
+		cout << "Unable to open file " << filename << endl;
+	}
+	
+	dpx::Reader dpx;
+	dpx.SetInStream(&img);
+	if (!dpx.ReadHeader())
+	{
+		cout << "Unable to read header" << endl;
+	}
+
+	// data size, override if user specifies
+	dpx::DataSize size = dpx.header.ComponentDataSize(0);
+	int nob = dpx.header.ComponentByteCount(0);
+	if (write8bit)
+	{
+		size = dpx::kByte;
+		nob = 1;
+	}
+
+	if (dpx.ReadImage(arr, size, dpx.header.ImageDescriptor(0)) == false)
+	{
+		cout << "unable to read image data " << size << endl;
+	}
+
+	img.Close();
+}
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    //
+    char* filename = mxArrayToString(prhs[0]);
+    
+    int height = 0;
+    int width = 0;
+    readDPXresolution(filename, height, width);
+    
+    int elements = height * width * 3;
+    vector<unsigned short> arr(elements,0);
+    
+	readDPX( filename, reinterpret_cast<char*>(&(arr[0])) );
+    
+    //
+    UINT16_T *dynamicData;
+    mwSize index, index_color, index_width, index_height;
+    
+    // Create a local array and load data
+    dynamicData = (UINT16_T*)mxCalloc(elements, sizeof(UINT16_T));
+
+    // save arr to dynamicData: 
+    index = 0;
+    for ( index_color = 0; index_color < 3; index_color++ )
+    {
+        for ( index_width = 0; index_width < width; index_width++ )
+        {
+            for ( index_height = 0; index_height < height; index_height++ )
+            { 
+                dynamicData[index] = arr[ (width*index_height+index_width)*3+index_color ];
+                index++;
+            }
+        }
+    }
+    
+    // Create a 3-D mxArray; you will allocate the memory dynamically
+    mwSize ndim = 3;
+    mwSize dims[] = {height,width,3};
+    plhs[0] = mxCreateNumericArray(ndim, dims, mxUINT16_CLASS, mxREAL);
+
+    // Point mxArray to dynamicData
+    mxSetData(plhs[0], dynamicData);
+    
+    int flag = mxSetDimensions(plhs[0], dims, ndim);
+    
+    // must be removed
+    // mxFree(dynamicData); 
+    
+}
